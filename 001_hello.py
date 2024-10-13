edce3be5acdae97e37214aff5e701d521565a271
AdrianLlontop <rizzaga1234@gmail.com>
2024-10-13 18:25:04 -0500

    tipo de dato

---
+++ 001_hello.py
@@ -0,0 +1,22 @@
+# Hola Mundo
+
+print("Hola Mundo")
+print('Hola Mundo')
+
+print(type("Hola Mundo"))
+
+"""
+Comentario entre comillas
+"""
+
+'''
+Comentario entre apostrofes
+'''
+
+# Consultar el tipo de dato
+
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
