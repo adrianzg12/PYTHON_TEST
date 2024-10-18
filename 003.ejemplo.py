67a7165f581d66a3c371baef30e1c180236d8c58
AdrianLlontop <rizzaga1234@gmail.com>
2024-10-18 11:54:06 -0500

    suma de numeros

---
+++ 003.ejemplo.py
@@ -0,0 +1,8 @@
+# Crear una lista de números
+numeros = [1, 2, 3, 4, 9]
+
+# Calcular la suma de los números
+suma = sum(numeros)
+
+# Imprimir el resultado
+print("La suma de los números es:", suma)
